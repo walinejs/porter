8473fd5a821061287e087575414e597b5b99c64a
Karuboniru <nico@niconi.org>
2021-09-27 16:27:54 +0800

    use cloudflare's cache and set a long max-age

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 });
 
+const cache = caches.default
+
 async function handleRequest(request) {
-  const url = new URL(request.url);
+  const url = new URL(request.url)
   const imageURL = url.searchParams.get('url');
   if(!imageURL) {
     return new Response(null, {status: 206});
   }
-
-  const imageRequest = new Request(imageURL, {headers: request.headers});
-  return fetch(imageRequest);
+  
+  let response = await cache.match(imageURL)
+  if (!response) {
+    const imageRequest = new Request(imageURL, {headers: request.headers});
+    response = await fetch(imageRequest)
+    response = new Response(response.body,{headers: response.headers})
+    response.headers.set("Cache-Control", "max-age=604800")
+    cache.put(imageURL, response.clone())
+  }
+  return response
 }
